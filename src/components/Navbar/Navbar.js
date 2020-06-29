0c59ba6c3efc48a96b7f79df4759cf605ab4aa66
David Odesola <faithodesola@gmail.com>
2020-06-30 04:27:52 +0800

    Adding the desktop appbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,14 +1,33 @@
 import React from "react";
 import { AppBar, Toolbar, Typography } from "@material-ui/core";
 import style from "./Navbar.module.css";
+import profile from "../../assets/images/davidO.jpg";
+import NotificationsIcon from '@material-ui/icons/Notifications';
 const Navbar = () => {
   return (
     <AppBar position="static" className={style.container}>
-      <Toolbar variant="dense">
+      <Toolbar variant="dense" className={style.toolbar}>
         <div className={style.brand}>
           <Typography variant="h4" color="inherit">
             Convey
           </Typography>
+          <input
+            className={style.searchInput}
+            placeholder="Search in Convey..."
+          />
+        </div>
+        <div className={style.utility}>
+            <NotificationsIcon/>
+          <div className={style.profile}>
+            <Typography
+              className={style.profileName}
+              variant="body2"
+              color="inherit"
+            >
+              David King
+            </Typography>
+            <img className={style.profileImage} src={profile} alt="profile" />
+          </div>
         </div>
       </Toolbar>
     </AppBar>
