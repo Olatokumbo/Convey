07ed35051d2fe88f96fe457b68eeff10acfa4339
David Odesola <faithodesola@gmail.com>
2020-06-30 06:04:28 +0800

    finishing the appbar layout

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,15 +2,14 @@ import React from "react";
 import { AppBar, Toolbar, Typography } from "@material-ui/core";
 import style from "./Navbar.module.css";
 import profile from "../../assets/images/davidO.jpg";
+import logo from "../../assets/images/logo.png";
 import NotificationsIcon from '@material-ui/icons/Notifications';
 const Navbar = () => {
   return (
     <AppBar position="static" className={style.container}>
       <Toolbar variant="dense" className={style.toolbar}>
         <div className={style.brand}>
-          <Typography variant="h4" color="inherit">
-            Convey
-          </Typography>
+        <img className={style.logo}src={logo} alt="logo"/>
           <input
             className={style.searchInput}
             placeholder="Search in Convey..."
